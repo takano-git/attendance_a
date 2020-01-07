e1a28225292bceb0580ced075ef41e033c33076c
takano-git <p.aaattt25@gmail.com>
2020-01-07 06:41:31 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページごとタイトルを返す
+  def full_title(page_name = "")
+    base_title = "AttendanceApp_A"
+    if page_name.empty?
+      base_title
+    else
+      page_name + " | " + base_title
+    end
+  end
 end
