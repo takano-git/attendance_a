634f79581625ecbd9ca459ab6619dcf1c15a1acb
takano-git <p.aaattt25@gmail.com>
2020-01-09 07:20:12 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,29 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
+  
+  def create
+    # モデルがユーザだから userというハッシュにキーと値のペアを入れているよ、と理解している
+    # シンボルはハッシュのキーとして使われているよ
+    # シンボルをキーにするとキーを""で囲わなくていいというメリットがあるよ　
+    # @user = User.new(params[:user])
+    # ↑これは悪い人が悪いことするかもしれないからストロングパラメーターを使う
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+    # redirect_to user_url(@user) これの省略形が下↓
+      redirect_to @user 
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
+  
 end
