43bcaa67932c31d3eb349e2d43a4e9c7f3eb61e2
takano-git <p.aaattt25@gmail.com>
2020-01-09 02:31:16 +0000

    Finish modeling-user

---
+++ db/migrate/20200108090855_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.boolean :admin, default: false
+      t.boolean :superior, default: false
+      t.string :department
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
