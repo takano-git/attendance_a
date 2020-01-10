c2345c8a5da8b29f7d241a07286205c75b3d8a1b
takano-git <p.aaattt25@gmail.com>
2020-01-10 06:35:47 +0000

    Finish login-function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,33 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+
+  # 1個目がparams[:session]になるのは,フォームで指定され送られてきたからと考えている
+  #        User.find_by(email: "sample@email.com")
+    user = User.find_by(email: params[:session][:email].downcase)
+    # Rubyではnilとfalse以外の全てのオブジェクトがtrueになるという性質がある
+    # 無いならnil password違えばfalse
+    if user && user.authenticate(params[:session][:password])
+      # ログイン後にユーザー情報ページにリダイレクトします。
+      # 引数に渡されたユーザーオブジェクトでログインします。
+      log_in user
+      redirect_to user
+    else
+      # renderhはリクエストではなくページをレンダリングしている。
+      # なので.nowがいる。
+      # redirect_toならいらないのだと思う
+      # flash.nowではレンダリングが終わっているページでフラッシュメッセージを表示することができます。
+      flash.now[:danger] = '認証に失敗しました。'
+      render :new
+    end
+  end
+  
+  def destroy
+    log_out
+    flash[:success] = 'ログアウトしました'
+    redirect_to root_url
+  end
+  
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,43 @@
 module SessionsHelper
+  
+  # 引数にわたされたユーザーオブジェクトでログインします。
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def log_out
+    # sessiomに保存されているuser_idを消去
+    session.delete(:user_id)
+    # @current_userにnilを代入
+    @current_user = nil
+  end
+  
+  # 現在ログイン中のユーザーがいる場合オブジェクトでログインします
+  # 一時的セッション内のユーザーIDをデータベースから取得出来るようにします
+  
+  # def current_user  
+  #   if session[:user_id]
+  #     if @current_user.nil?
+        # find_byならnilだった場合でも、例外処理は発生せずにnilが返されます。
+  #       @current_user = User.find_by(id: session[:user_id])
+  #     else
+  #       @current_user
+  #     end
+  #   end
+  # end
+  
+  def current_user
+    if session[:user_id]
+    # @current_user = @current_user || User.find_by(id: session[:user_id])
+      @current_user ||=  User.find_by(id: session[:user_id])
+    end
+  end
+  
+  # 現在ログイン中のユーザーがいればtrue,そうでなければfalseを返します。
+  # セッションにuser_idが入ってないか@current_userにユーザーオブジェクトが入っていない
+  
+  def logged_in?
+    !current_user.nil?
+  end
+  
 end
