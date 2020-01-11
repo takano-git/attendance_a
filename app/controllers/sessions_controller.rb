88c2ce4a3e1c0b644050f23377864540084b19e8
takano-git <p.aaattt25@gmail.com>
2020-01-11 22:55:28 +0000

    Add Frendly Forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       # 引数に渡されたユーザーオブジェクトでログインします。
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       # renderhはリクエストではなくページをレンダリングしている。
       # なので.nowがいる。
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
-   @user = User.find(params[:id])
   end
   
   def new
@@ -26,13 +28,12 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # 更新に成功した場合の処理を記述
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
     else
       render :edit
     end
@@ -43,5 +44,25 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
+
+  # before フィルター
   
+  # paramsハッシュからユーザーを取得します。
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  # ログイン済みのユーザーか確認します。
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] ="ログインしてください"
+      redirect_to login_url
+    end
+  end
+
+ # ログイン済みのユーザーか確認します。
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current.user?(user)
+  end
 end
