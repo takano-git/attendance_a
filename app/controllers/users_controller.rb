db71c96352c0a39f7937b367cef0e388c69b0cc7
takano-git <p.aaattt25@gmail.com>
2020-01-15 04:26:07 +0000

    ユーザー一覧ページのパーシャル追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
+  def index
+    @users = User.all
+  end
+  
   def show
   end
   
@@ -55,6 +59,7 @@ class UsersController < ApplicationController
   # ログイン済みのユーザーか確認します。
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] ="ログインしてください"
       redirect_to login_url
     end
@@ -62,7 +67,7 @@ class UsersController < ApplicationController
 
  # ログイン済みのユーザーか確認します。
   def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless @user == current.user?(user)
+    # @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
