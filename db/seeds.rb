da11358f0bd157a509b52402669143a91328a4c3
takano-git <p.aaattt25@gmail.com>
2020-01-16 02:57:52 +0000

    Finish japanization-and-time-format

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,34 @@
 
 # coding: utf-8
 
-User.create!(name: "Admin User",
+User.create!(name: "Admin",
              email: "sample@email.com",
              password: "password",
              password_confirmation: "password",
              admin: true)
              
-User.create!(name: "Superior User 1",
+User.create!(name: "Superior1",
              email: "Superior1@email.com",
              password: "password",
              password_confirmation: "password",
              superior: true)
 
-User.create!(name: "Superior User 2",
+User.create!(name: "Superior2",
              email: "Superior2@email.com",
              password: "password",
              password_confirmation: "password",
              superior: true)
+
+User.create!(name: "Staff1",
+             email: "staff1@email.com",
+             password: "password",
+             password_confirmation: "password")
              
+User.create!(name: "Staff2",
+             email: "staff2@email.com",
+             password: "password",
+             password_confirmation: "password")
+
 10.times do |n|
   name = Faker::Name.name
   email ="sample-#{n+1}@email.com"
